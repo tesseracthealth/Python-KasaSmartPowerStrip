51e2cf8ee36540c8983defa0df3fafcc6d1acba2
dav lion <david.lion@gmail.com>
2021-02-03 18:22:51 -0800

    add packaging support

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='KasaSmartPowerStrip',
+    version='1.0',
+    packages=[''],
+    url='git@github.com:tesseracthealth/Python-KasaSmartPowerStrip.git',
+    license='Apache v2.0',
+    author='',
+    author_email='dlion@tesseracthealth.com',
+    description='fork of upstream Kasa Power Plug lib'
+)
